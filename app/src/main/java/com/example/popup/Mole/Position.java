a1f8add46b8868bed647498eb6fc6ebd1a0bbd7c
elitekalkulator <61491389+elitekalkulator@users.noreply.github.com>
2020-04-08 18:21:51 -0700

    ok

--- app/src/main/java/com/example/popup/Mole/Position.java
+++ app/src/main/java/com/example/popup/Mole/Position.java
@@ -1,7 +1,27 @@
 package com.example.popup.Mole;
 
-public class Position {
-    private double x, y;
+public class nice {
+    public static void main(String[ ] args) {
+        Position mole = new Position(1,2);
+        System.out.println(mole.getX());
+        System.out.println(mole.getY());
+    }
 }
 
-// nice
+class Position{
+    private double x;
+    private double y;
+
+    public Position(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+    public double getY(){
+        return y;
+    }
+}
