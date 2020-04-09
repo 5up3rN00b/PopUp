d160ae72441e4bb55784a1e66053577d6580a781
5up3rN00b <aaron.linear@gmail.com>
2020-04-08 18:09:11 -0700

    Debugged code

--- app/src/main/java/com/example/popup/Board.java
+++ app/src/main/java/com/example/popup/Board.java
@@ -11,7 +11,7 @@ public class Board {
     private static Board b;
 
     private Board() {
-        ArrayList<Mole> moles = new ArrayList<Mole>;
+        ArrayList<Mole> moles = new ArrayList<>();
     }
 
     public static Board getInstance() {
@@ -22,27 +22,18 @@ public class Board {
         return b;
     }
 
+    public void timedBirth() {
+        
+    }
+
     // Runs when there is a click
     public void click() {
-        int index = -1;
-        boolean died = false;
-        for (Mole a : moles){ //traverses array to see if any are Hit, if so, call death, score ++
-            index ++;
-            if (a.isHit) {
+        for (int i = 0; i < moles.size(); i++){ //traverses array to see if any are Hit, if so, call death, score ++
+            Mole m = moles.get(i);
+            if (m.isHit()) {
                 score++;
-                a.death();
-                break;
+                m.death();
             }
-
         }
-
-        if (index != moles.size()){ //if any were hit, remove it from arraylist and birth a new one
-            moles.remove(index);
-            Mole a = new Mole ();
-            a.birth();
-            moles.add(index, a);
-
-        }
-
     }
 }
--- app/src/main/java/com/example/popup/Mole/SuperMole.java
+++ app/src/main/java/com/example/popup/Mole/SuperMole.java
@@ -2,15 +2,16 @@ package com.example.popup.Mole;
 
 public class SuperMole extends Mole {
     private int lives;
-    public SuperMole(){
+
+    public SuperMole(float x, float y) {
         super(x, y);
         lives = 2;
     }
 
     public void death() {// decrease life by 1 everytime it's clicked
         lives--;
-        if(lives == 0){
-            layer.remove();
+        if (lives == 0){
+            super.death();
         }
     }
 }
